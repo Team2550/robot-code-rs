2e135f0c80467a059b590677467d6c54daef0335
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2023-11-12 18:07:55 -0800

    Implement joystick X & Y
    I'm unsure of the implementation, needs testing.

--- src/input.rs
+++ src/input.rs
@@ -19,14 +19,19 @@ trait Input {
 
   const A: u8 = 1;
   const B: u8 = 2;
-	const X: u8 = 3;
-	const Y: u8 = 4;
+  const X: u8 = 3;
+  const Y: u8 = 4;
 
   const LB: u8 = 5;
   const RB: u8 = 6;
 
   const BACK: u8 = 7;
   const START: u8 = 8;
+	
+  const LeftX: u8 = 0; // To-Do: Check that these are the correct values for said stick.
+  const LeftY: u8 = 1;
+  const RightX: u8 = 2;
+  const RightY: u8 = 3;
 
 }
 
@@ -36,12 +41,12 @@ struct XboxController<'a> {
 }
 
 impl XboxController<'_> {
-  fn y(&self) -> f32 {
+  fn raw_axis(&self, axis: u8) -> f32 {
       self.ds
-          .stick_axis(self.port, JoystickAxis::new(1).unwrap())
+          .stick_axis(self.port, JoystickAxis::new(axis).unwrap())
           .unwrap_or(0.0)
   }
-
+	
   fn raw_button(&self, button: u8) -> bool {
       self.ds.stick_button(self.port, button).unwrap_or(false)
   }
@@ -70,11 +75,19 @@ fn boost(&self) -> bool {
     self.raw_button(Self::RB)
   }
 
-  fn lefttankaxis(&self) -> f32 {
-    self.y() //Figure out how to specify which stick
+  fn leftx(&self) -> f32 {
+    self.raw_axis(Self::LeftX)
   }
 
-  fn righttankaxis(&self) -> f32 {
-    self.y() //Figure out how to specify which stick
+  fn lefty(&self) -> f32 {
+    self.raw_axis(Self::LeftY) 
+  }
+
+  fn rightx(&self) -> f32 {
+    self.raw_axis(Self::RightX)
+  }
+	
+  fn righty(&self) -> f32 {
+    self.raw_axis(Self::RightY)
   }
 }
